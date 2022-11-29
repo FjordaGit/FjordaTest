a1d7db0190767459b8dbbc1c521e3de405e82f19
Fjorda <101427500+FjordaGit@users.noreply.github.com>
2022-11-29 21:08:04 +0000

    Scene Transitions

---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public void NextScene(string sceneName)
+    {
+      
+        SceneManager.LoadScene(sceneName);
+    }
+
+}

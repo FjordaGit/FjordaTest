af15e5185f45b6e560ef7fa4dc46a6d46756677d
Fjorda <101427500+FjordaGit@users.noreply.github.com>
2022-11-29 23:50:41 +0000

    Scene 2 Completed

---
+++ Assets/Scripts/AnimOnBtnClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimOnBtnClick : MonoBehaviour
+{
+    public Animator BtnAnim;
+    public Image TextModelImage;
+    public Sprite NewTextModelImage;
+
+    void OnMouseDown()
+    {
+        BtnAnim.enabled = true;
+        TextModelImage.sprite = NewTextModelImage;
+    }
+
+
+}

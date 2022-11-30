4416677b5c3694e0aafce9f6b2395bb58d19e401
Fjorda <101427500+FjordaGit@users.noreply.github.com>
2022-11-30 18:56:52 +0000

    Scene 1 Completed

---
+++ Assets/Scripts/GetDataApi.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+
+public class GetDataApi : MonoBehaviour
+{
+    public string thumbnailUrl;
+    public Image image;
+
+    void Start()
+    {
+        // Debug.Log(thumbnailUrl);
+        image = GetComponentInChildren<Image>();
+
+
+        StartCoroutine(GetTexture(thumbnailUrl));
+    }
+
+    IEnumerator GetTexture(string url)
+    {
+
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("@@@");
+            Texture2D myTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            image.color = Color.red;
+
+            Sprite newSprite = Sprite.Create(myTexture, new Rect(0, 0, myTexture.width, myTexture.height), new Vector2(.5f, .5f));
+            image.sprite = newSprite;
+        }
+    }
+}
---
+++ Assets/Scripts/VerticalScrollVeiw.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class VerticalScrollVeiw : MonoBehaviour
+{
+    [System.Serializable]
+    public class Data
+    {
+        public int id;
+        public string title;
+        public string thumbnailUrl;
+    }
+
+    [System.Serializable]
+    public class DataList
+    {
+        public Data[] data;
+    }
+
+    public Transform scrollViewPos;
+    public GameObject ScrollLine1;
+    public GameObject ScrollLine2;
+    public string textUrl;
+
+    public DataList mDataList = new DataList();
+
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    private IEnumerator GetText()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(textUrl))
+        {
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                Debug.Log("Successfully download text");
+
+                var text = request.downloadHandler.text;
+                //Debug.Log(text);
+                text = "{\"data\":" + text + "}";
+                mDataList = JsonUtility.FromJson<DataList>(text);
+
+                if (mDataList != null)
+                {
+                    yield return StartCoroutine(SetListView()); ;
+                }
+
+            }
+
+        }
+    }
+
+    private IEnumerator SetListView()
+    {
+        Vector3 position = new Vector3(0, 0, 0);
+        int i = 0;
+        foreach (Data data in mDataList.data)
+        {
+            //Debug.Log(data.title);
+            if (i >= 20) break;
+
+            GameObject obj;
+            if (i % 2 == 0)
+            {
+                obj = Instantiate(ScrollLine1);
+            }
+            else
+            {
+                obj = Instantiate(ScrollLine2);
+            }
+            obj.GetComponent<GetDataApi>().thumbnailUrl = data.thumbnailUrl;
+            obj.GetComponentInChildren<Text>().text = data.title;
+
+            obj.transform.SetParent(scrollViewPos);
+            obj.transform.localPosition = position;
+            position.y = obj.transform.localPosition.y;
+            i++;
+
+        }
+        yield return null;
+    }
+}
